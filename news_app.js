8a7f32720e3a501ef2f94cb52617fa092ed629d5
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-10-26 18:59:13 -0400

    Create the index page for the News App

--- news_app.js
+++ news_app.js
@@ -1,1 +1,13 @@
-document.getElementById('card')
+var button = document.getElementById('button');
+var url = 'http://newsapi.org/v2/top-headlines?' +
+'country=us&' +
+'apiKey=5b70edca1d034d86b9a94213f1344c61';
+var req = new Request(url);
+fetch(req)
+.then(function(response) {
+    console.log(response.json());
+})
+
+
+
+
