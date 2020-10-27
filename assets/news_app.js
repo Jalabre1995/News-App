70cfd77596832f8138e26338fb6becd4d300769c
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-10-27 16:47:42 -0400

    redoing the git response to ge the sources forthe artciles

--- assets/news_app.js
+++ assets/news_app.js
@@ -1,16 +1,16 @@
 /// created a variable to hold the total number of news articles
-let totalNews = 0;
+let totalSources = 0;
 
 //Increment if the query was successful have it start at 0.
 
 let randomNewsCount = 0;
 
-// Query the top articles right here
+// Query all the sources right here
 $.ajax({
-    url: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=5b70edca1d034d86b9a94213f1344c61',
+    url: 'https://newsapi.org/v2/sources?apiKey=5b70edca1d034d86b9a94213f1344c61',
     method: 'GET'
 }).then(function(response) {
-    totalNews = response.count;
+    totalSources = response.count;
     prepDisplay();
     ///Show the necessary <divs> and 
     $('#news-search-results').removeClass('d-none');
@@ -30,6 +30,80 @@ function prepDisplay(){
 }
 
 ///Get the news articles///
+function getNewsArticles(){
+    let randNum = Math.floor(Math.random() * totalNews);
+    $.ajax({
+        url: 'https://newsapi.org/v2/everything?q=' + randNum,
+        method: 'Get'
+    }).then(function(response){
+        populateNewsSearchCards(response);
+        randomNewsCount++;
+        //call the fucntion until 5 responses are generated
+        if (randomNewsCount < 5 ) {
+            getNewsArticles();
+        }
+        ///If we get an 404 error then don't increment the resposne//
+    }).fail(function() {
+        getNewsArticles();
+    });
+}
+///When the user loades the page, have the div cards populated and loaded
+
+function populateNewsSearchCards(resposne){
+    let newCard = $('<div class="card"');
+    $(newCard).attr('id', response.id);
+    let cardImage = $('<div class="image"');
+    if(response.background_imge == null){
+        ///Put a radom placeholder 
+    }else{
+        $(cardImage).append('<img src="' + response.background_imge + '">');
+    }
+    newCard.append(cardImage);
+
+    let cardContent = $('<div class="content">');
+    let cardHeader = $('<div class-="header">').text(response.name);
+    cardContent.append(cardHeader);
+
+    let description = $('<div class="description">');
+
+    let releaseDate = $('<p>').text('Release Date: ' + response.realeased);
+    description.append(releaseDate);
+
+    let articleId = $('<p>').text('News ID: ' + response.id);
+    description.append(articleId);
+    
+    cardContent.append(description);
+    newCard.append(cardContent);
+
+    $('#news-cards').append(newCard);
+
+}
+
+///Using the event handler///
+var searchIcon = $("#search-button");
+searchIcon.on('click', function(e)
+{
+    e.preventDefault();
+    prepDisplay();
+    $('#news-search-results').removeClass('d-none');
+    var userInput = $(".searchInput").val();
+    uSearch(userInput)
+
+})
+
+function uSearch(title) {
+    $.ajax({
+        url: "'https://newsapi.org/v2/everything?page_size=5&search=" + title,
+        method: 'GET'
+    }).then(function(response) {
+        for (var i = 0; i < response.results.length; i++)
+        {
+            populateNewsSearchCards(response.results[i]);
+        }
+    });
+}
+
+
 
 
 
@@ -40,5 +114,25 @@ function queryNews(news) {
         mwthod: 'GET'
     }).then(function(response) {
         $('#news-header').text("Here is" + response.articles.length + 'news related to ' + news);
+
+        if(response.articles.legnth > 0) {
+            ///Loop through all the articles that are given
+            for(let i = 0; i < response.articles.length; i++) {
+                //populoate the news results using semantic UI
+                let newItem = $('<div class= "item">');
+                let itemImage = $('div class= "image">');
+                $(itemImage).append('<img src="' + response.articles[i].urlToImage + '">');
+                newItem.append(itemImage);
+
+                let itemContent =  $('<div class="content">');
+                let itemHeader = $('<div class="header">').text(response.articles[i].title);
+                itemContent.append(itemHeader);
+
+                let description = $('<div class="description"');
+                $(description).appned($('<p>').text('Published by ' + response.articles[i].source.name + ' at ' + response.articles[i].publishedAt));
+                $(description).appedn($('<p>').text(response.articles[i].description))
+                itemContent.append(description);
+            }
+        }
     })
 }
